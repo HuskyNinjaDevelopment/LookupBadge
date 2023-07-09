802a92f41fffc4ffa6d6d121639e0d3301db5421
HuskyNinjaDevelopment <HuskyNinjaDevelopment@gmail.com>
2023-07-08 20:10:59 -0400

    Add project files.

---
+++ Lookup.cs
@@ -0,0 +1,98 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using FivePD.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookupBadge
+{
+    internal class Lookup: Plugin
+    {
+        internal Lookup()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            API.RegisterCommand("lookup", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                //Either no input for more than the badge was given
+                if(args.Count != 1) { ShowError("input"); return; }
+
+                //Put the badge number into a variable and strip any dashes
+                string badge = args[0].ToString();
+                if(badge.Contains("-")) { badge = badge.Replace("-", ""); }
+
+                //Validate badge number, checking length and if the first element is a letter or digit, make sure 3 remaining chars are digits
+                if(badge.Length > 4) { ShowError("badge"); return; }
+                if (!char.IsLetterOrDigit(badge[0])) { ShowError("badge"); return; }
+                for(int i = 1; i < badge.Length; i++) //Skip the first element as it has already been validated
+                {
+                    if (!char.IsDigit(badge[i])) { ShowError("badge"); return; }
+                }
+
+                //Valid Badge trigger lookup on server
+                TriggerEvent("chat:addMessage", new 
+                { 
+                    color = new[] {255, 0, 0},
+                    multiline = true,
+                    args = new[] {"LookUp", $"Getting data on badge {badge}..."}
+                });
+                await Delay(1500);
+                TriggerServerEvent("LookUp:Badge", badge);
+            }), false);
+
+            TriggerEvent("chat:addSuggestion", "/lookup", "/lookup [NNNN or L-NNN] will return information on the officer with that callsign.");
+
+            EventHandlers["LookUp:Success"] += new Action<string, string>(LookUpSuccess);
+            EventHandlers["LookUp:Failure"] += new Action<string>(LookUpFailure);
+        }
+
+        private void LookUpSuccess(string data, string badge)
+        {
+            ShowNotification("~g~LookUp Success");
+            TriggerEvent("chat:addMessage", new 
+            { 
+                color = new[] {255, 0, 0},
+                multiline = true,
+                args = new[] {"LookUp", $"The badge {badge}~s~ belongs to {data}."}
+            });
+        }
+        private void LookUpFailure(string badge)
+        {
+            ShowNotification("~r~LookUp Failed");
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 255, 0, 0 },
+                multiline = true,
+                args = new[] { "LookUp", $"~r~Failed~s~ to find information on ~b~{badge}." }
+            });
+        }
+
+        private void ShowError(string fault)
+        {
+            switch(fault)
+            {
+                case "input":
+                    ShowNotification("~r~Invalid Input");
+                    break;
+                case "badge":
+                    ShowNotification("~r~Invalid Badge Number");
+                    break;
+                default:
+                    ShowNotification("~r~Error: Lookup Failed");
+                    break;
+            }
+        }
+        private void ShowNotification(string msg)
+        {
+            API.SetNotificationTextEntry("STRING");
+            API.AddTextComponentString(msg);
+            API.DrawNotification(true, true);
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,99 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Server: BaseScript
+    {
+        private string connString;
+
+        public Server()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            LoadConfig();
+            EventHandlers["LookUp:Badge"] += new Action<Player, string>(LookupBadge);
+        }
+        private void LoadConfig()
+        {
+            string test = API.LoadResourceFile(API.GetCurrentResourceName(), "config.json");
+            var config = JObject.Parse(test);
+
+            connString = config["connectionString"].ToString();
+        }
+        private async void LookupBadge([FromSource] Player source, string badgeNumber)
+        {
+            MySqlConnection conn;
+            MySqlCommand cmd;
+
+            conn = new MySqlConnection();
+            cmd = new MySqlCommand();
+
+            conn.ConnectionString = connString;
+
+            try
+            {
+                conn.Open();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "SELECT userID FROM fivepd.department_members WHERE callsign = @badge;";
+                cmd.Parameters.AddWithValue("@badge", badgeNumber.ToString());
+                MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+
+                //Give time for the query to return
+                await BaseScript.Delay(50);
+
+                if (reader.HasRows)
+                {
+                    string id = reader.GetValue(0).ToString();
+                    reader.Close();
+
+                    cmd.CommandText = "SELECT gameName FROM fivepd.users WHERE id = @userID;";
+                    cmd.Parameters.AddWithValue("@userID", id);
+
+                    reader = cmd.ExecuteReader();
+                    reader.Read();
+
+                    await Delay(50);
+
+                    if(reader.HasRows)
+                    {
+                        source.TriggerEvent("LookUp:Success", reader.GetValue(0).ToString(), badgeNumber.ToString());
+                    }
+                    else
+                    {
+                        source.TriggerEvent("LookUp:Failure", badgeNumber.ToString());
+                    }
+                    reader.Close();
+                }
+                else
+                {
+                    source.TriggerEvent("LookUp:Failure", badgeNumber.ToString());
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                source.TriggerEvent("LookUp:Failure", badgeNumber.ToString());
+
+                Debug.WriteLine("");
+                Debug.WriteLine("~r~LookUp Error~s~:");
+                Debug.Write(ex.Message);
+                Debug.WriteLine("");
+            }
+
+        }
+    }
+}
